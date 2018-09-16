fd139c1c469c2d4f6fd1f886f1958807873406a4
Luis Jose Sanchez <luisjoseprofe@gmail.com>
2018-09-16 16:27:55 +0200

    TodoEdit page and Todo service added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'edit', loadChildren: './edit-todo/edit-todo.module#EditTodoPageModule' },
+  { path: 'edit/:id', loadChildren: './edit-todo/edit-todo.module#EditTodoPageModule' },
 ];
 
 @NgModule({
---
+++ src/app/edit-todo/edit-todo.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-todo',
+  templateUrl: './edit-todo.page.html',
+  styleUrls: ['./edit-todo.page.scss'],
+})
+export class EditTodoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
