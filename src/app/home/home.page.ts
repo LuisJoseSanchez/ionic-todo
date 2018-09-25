3dab052f5bc6bff611ed613cd02cd113e11a36ce
Luis Jose Sanchez <luisjoseprofe@gmail.com>
2018-09-25 17:35:47 +0200

    Navigate to edit tasks from component.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 import { Todo } from '../interfaces/todo';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -11,9 +12,20 @@ export class HomePage implements OnInit {
 
   todos: Todo[] = [];
 
-  constructor(private todoService: TodoService) { }
+  constructor(
+    private todoService: TodoService,
+    private navControler: NavController
+  ) { }
 
   ngOnInit() {
     this.todos = this.todoService.getTodos();
+  }
+
+  goEditTodo(id: number) {
+    this.navControler.navigateForward('/edit/' + id);
+  }
+
+  deleteDialog(id: number) {
+
   }
 }
